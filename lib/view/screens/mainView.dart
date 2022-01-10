019788090b56c535cc12202f00b26c7facc5332f
cengizhan <cengiz_er_@hotmail.com>
2022-01-10 16:11:39 +0300

    finished

--- lib/view/screens/mainView.dart
+++ lib/view/screens/mainView.dart
@@ -1,7 +1,10 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:number_facts/core/facts.dart';
 import 'package:number_facts/services/facts_service.dart';
+import 'package:number_facts/view/screens/AboutView.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class MainView extends StatefulWidget {
@@ -10,19 +13,25 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
-  // ! START OF THE TREE
+  
   @override
   void initState() {
     super.initState();
   }
 
   final _factsResponseService = FactsResponseService();
+  int _currentindex = 0;
 
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {                       // ! START OF THE TREE
     final deviceData = MediaQuery.of(context);
     return Scaffold(
+      appBar: AppBar(
+        shadowColor: Colors.transparent,
+        backgroundColor: Colors.black,
+      ),
+      endDrawer: myDrawerWidget(),
       body: Container(
-        decoration:BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/background.jpg"),
             fit: BoxFit.cover,
@@ -42,8 +51,44 @@ class _MainViewState extends State<MainView> {
           ],
         ),
       ),
+      bottomNavigationBar: SalomonBottomBar(
+        unselectedItemColor: Colors.black,
+        currentIndex: _currentindex,
+        onTap: (i) => setState(() {
+          _currentindex = i;
+          if (_currentindex == 1) {
+            _currentindex=0;
+            Navigator.of(context).push(
+              MaterialPageRoute<void>(
+                builder: (BuildContext context) => AboutView(),
+              ),
+            );
+          }
+          if (_currentindex == 2) {
+            SystemNavigator.pop();
+          }
+        }),
+        items: [
+          SalomonBottomBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+            selectedColor: Colors.red,
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(Icons.fiber_new_sharp),
+            title: Text("Daily"),
+            selectedColor: Colors.red,
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(Icons.exit_to_app_sharp),
+            title: Text("Exit"),
+            selectedColor: Colors.red,
+          ),
+        ],
+      ),
     );
-  } // ! END OF THE TREE
+  }                                                        // ! END OF THE TREE
+
 
   FutureBuilder<Facts> buildFutureBuilderMainDummie() {
     return FutureBuilder<Facts>(
@@ -63,6 +108,8 @@ class _MainViewState extends State<MainView> {
             return Center(
               child: Text("its active"),
             );
+          default:
+            return Center(child: Text("hata"));
         }
       },
     );
@@ -90,7 +137,7 @@ class _MainViewState extends State<MainView> {
             height: 50,
           ),
           CircularStepProgressIndicator(
-            totalSteps: 20,
+            totalSteps: 10,
             width: MediaQuery.of(context).size.width * 0.2,
             height: MediaQuery.of(context).size.height * 0.1,
             gradientColor: LinearGradient(
@@ -149,7 +196,7 @@ class _MainViewState extends State<MainView> {
       child: Text(
         "GET A RANDOM FACT",
         style: TextStyle(
-          color: Colors.white,
+          color: Colors.black,
           fontWeight: FontWeight.bold,
           fontStyle: FontStyle.italic,
         ),
@@ -158,5 +205,78 @@ class _MainViewState extends State<MainView> {
         setState(() {});
       },
     );
+  }
+}
+
+class myDrawerWidget extends StatelessWidget {
+  const myDrawerWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        backgroundColor: Colors.black,
+        child: Container(
+            child: ListView(
+              children: [
+                ListTile(
+                  leading: Icon(
+                    Icons.verified_user,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    "API copyrights",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  subtitle: Text("www.numbersapi.com/",
+                      style: TextStyle(color: Colors.green)),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.functions,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    "Used methods",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  subtitle: Text(
+                    "HTTP - JSON",
+                    style: TextStyle(color: Colors.green),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.create,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    "Creators",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  subtitle: Text(
+                    "Students of Ege University",
+                    style: TextStyle(color: Colors.green),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.class_,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    "Section Info",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  subtitle: Text(
+                    "Mobile Programming",
+                    style: TextStyle(color: Colors.green),
+                  ),
+                ),
+              ],
+            ),
+            color: Colors.black),
+      );
   }
 }
